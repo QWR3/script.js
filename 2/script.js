6fa45e775e39071e6968dbd38f0abb55dbd1e98f
QWR3 <anton7@i.ua>
2020-10-29 17:05:30 +0200

    Initial commit

---
+++ 2/script.js
@@ -0,0 +1,297 @@
+// 1
+// let a = [5 , 6 , 7, 8 , 9 ];
+// console.log(a)
+// let a = [ 'a' , 'b' , 'c' , 'd' , 'e'];
+// console.log(a)
+// let a = ['1' , '2' , 3 , 4 , true]
+// console.log(a)
+
+// 2
+// let a = ['S' ,'U' ,'R' ,'S'];
+// console.log(a [1]);
+// console.log(a [3]);
+// console.log(a [0]);
+// console.log(a [2]);
+
+// 3
+// for (let a=0 ; a < 10 ; a++){
+//     document.write('<h1>NARUTO</h1> </br>')
+// }
+
+// 4
+// for (let a=0;a<10;a++){
+//     document.write(`<div>NARUTO ${a}</div> </br>`)
+// }
+
+// 6
+// let start = 0;
+// while (start < 20) {
+// 	document.write(`<h1> NARUTO</h1>`);
+// 	start++;
+// }
+
+// 7
+// let start = 0;
+// while (start < 20) {
+//     document.write(`<h1> NARUTO ${start}</h1>`);
+//     start++;
+// }
+
+// 8
+// for(let i=0;i<10;i++){
+//     let x = [1,2,3,4,5,6,7,8,9,10];
+//     console.log(x)
+// }
+
+// 9
+// for(let i=0;i<10;i++){
+//let x = ['1','2','3','4','5','6','7','8','9','10'];
+//console.log(x)
+//}
+
+// 10
+// for(let i=0;i<10;i++){
+// let x = [true,'2',3,'4',5,'6',7,'8',9,'10'];
+// console.log(x)
+// }
+
+// 11
+// const x = [true,'2',3,'4',5,'6',7,'8',9,false];
+// for (let i = 0 ; i < 10 ; i++){
+//     if (typeof x[`${i}`] == 'boolean'){
+//         console.log(x[`${i}`])
+//     }
+
+// 12
+// const x = [true,'2',3,'4',5,'6',7,'8',9,false];
+// for (let i = 0 ; i < 10 ; i++){
+//     if (typeof x[`${i}`] == 'number'){
+//         console.log(x[`${i}`])
+//     }
+// }
+
+// 13
+// const x = [true,'2',3,'4',5,'6',7,'8',9,false];
+// for (let i = 0 ; i < 10 ; i++){
+//     if (typeof x[`${i}`] == 'string'){
+//         console.log(x[`${i}`])
+//     }
+// }
+
+// 14
+// const x = [true,'2',3,'4',5,'6',7,'8',9,false];
+// for (let i = 0 ; i < 10 ; i++){
+//     console.log(x[`${i}`])
+//}
+
+// 15
+// for (let i = 0 ; i < 10 ; i++){
+//     document.write( `${i}</br>`);
+//     console.log(`${i}`);
+// }
+
+// 16
+// for (let i = 0 ; i < 100 ; i++){
+//     document.write( `${i}</br>`);
+//         console.log(`${i}`);
+// }
+
+//17
+// for (let i = 0 ; i < 100 ; i += 2){
+//     document.write( `${i}</br>`);
+//         console.log(`${i}`);
+//}
+
+//18
+// for (let i = 0 ; i < 100 ; i++){
+//     if (`${i}` % 2 == 0){
+//         document.write(`${i}</br>`);
+//         console.log(`${i}`)
+//     }
+// }
+
+//19
+// for (let i = 0 ; i < 100 ; i++){
+//     if (`${i}` % 2 == 0){
+//         i++
+//         document.write(`${i}</br>`);
+//         console.log(`${i}`)
+//     }
+// }
+
+//20
+// for (let m = 0;m < 2 ; m++){
+//     for (let s = 0 ;  s <= 59;s++){
+//         document.write(`<h1>${m} m. : ${s}s.</h1> </br>`);
+// }}
+
+//21 як зробити 2 з половиною години)?
+// for (let h = 0 ; h <= 2 ; h++){
+//     for (let m=0 ; m <=59 ; m++){
+//         for (let s=0 ; s <=59 ; s++){
+//             document.write(`<h1>${h} h. : ${m} m. : ${s}s. </h1></br>`)
+//         }
+//     }
+// }
+
+//22
+// let a = ['a' , 'b' , 'c']
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     document.write(`${aElement}`)
+// }
+
+//23
+// let a = ['a' , 'b' , 'c']
+// let i = 0 ;
+// while (i<a.length){
+//     const aElement = a[i];
+//     document.write(`${aElement}`)
+//     i++
+// }
+
+// 24
+// let a = ['a' , 'b' , 'c']
+// for (const string of a) {
+//     document.write(string)
+// }
+
+// 25
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// let i = 0;
+// while (i<a.length){
+//     const aElement = a[i];
+//     document.write(`${aElement},`)
+//     i++
+// }
+
+// 26
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     document.write(`${aElement},`)
+//}
+
+// 27
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     if (aElement % 2 !== 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+
+// 28
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     if (aElement % 2 !== 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+
+// 29
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     if (aElement % 2 === 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+
+// 30
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     if (aElement % 2 === 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+
+// 31
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = 0; i < a.length; i++) {
+//     const aElement = a[i];
+//     if (aElement % 3 === 0){
+//        let aElement ='octen'
+//         console.log(aElement)
+//     }
+// }
+
+//32 чому 1 undefined
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >= 0; i--) {
+//     const aElement = a[i];
+//
+//     document.write(`${aElement},`)
+//
+// }
+
+//33
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >=0; i--) {
+//     const aElement = a[i];
+//     if (aElement % 3 === 0){
+//        let aElement ='octen'
+//         console.log(aElement)
+//     }
+// }
+//
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >=0; i--) {
+//     const aElement = a[i];
+//     if (aElement % 2 === 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+//
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >=0; i--) {
+//     const aElement = a[i];
+//     if (aElement % 2 === 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+//
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >=0; i--) {
+//     const aElement = a[i];
+//     if (aElement % 2 !== 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+//
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >=0; i--) {
+//     const aElement = a[i];
+//     if (aElement % 2 !== 0){
+//         document.write(`${aElement},`)
+//     }
+// }
+//
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// for (let i = a.length; i >=0; i--) {
+//     const aElement = a[i];
+//     document.write(`${aElement},`)
+//}
+//
+// let a =[2,17,13,6,22,31,45,66,100,-18];
+// let i = a.length;
+// while (i>=0){
+//     const aElement = a[i];
+//     document.write(`${aElement},`)
+//     i--
+// }
+
+// 24
+// let a = [];
+// let a2 = []
+// for (let i = 0; i < 50; i++) {
+//     if (i%2 === 0){
+//         a[i] = i
+//     }
+//     if (i%2 !== 0){
+//         a2[i] = i
+//     }
+//
+// }
