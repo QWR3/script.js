4bf8f075bac5f95cdf111fd71b17d06b289b248b
QWR3 <anton7@i.ua>
2020-10-29 17:05:30 +0200

    Initial commit

---
+++ 1/script.js
@@ -0,0 +1,297 @@
+// 1 завдання
+// let name ='viktor';
+// let hello ='hi';
+// let owu ='owu';
+// let com ='Ukraine.com';
+// let one = 1;
+// let ten = 10;
+// let MinusOneThousand = -999;
+// let pi = 3.14;
+// let three = 2.7
+// let sixteen = 16;
+// let t = 'true';
+// let f = 'false';
+// alert(name);
+// alert(hello);
+// alert(owu);
+// document.write(com );
+// document.write(one );
+// document.write(ten );
+// document.write(MinusOneThousand );
+// console.log(pi);
+// console.log(three);
+// console.log(sixteen);
+// console.log(t);
+// console.log(f);
+
+// 2 завдання
+// let name ='vasya';
+// let hello ='-.-';
+// let owu ='=.=';
+// let com =',;,0,;,_';
+// let one = '1cow_';
+// let ten = '10cow_';
+// let MinusOneThousand = 'cowsay\'-999\'_';
+// let pi = 'pipi';
+// let three = '-4°'
+// let sixteen = 'big boy';
+// let t = 'yes';
+// let f = 'not';
+// alert(name);
+// alert(hello);
+// alert(owu);
+// document.write(com );
+// document.write(one );
+// document.write(ten );
+// document.write(MinusOneThousand );
+// console.log(pi);
+// console.log(three);
+// console.log(sixteen);
+// console.log(t);
+// console.log(f);
+
+//3 завдання
+// const P = 3.14;
+// const one = 1;
+// const age = 40;
+// const name = 'Joseph';
+// const job = 'leader';
+// const IsWife = 'yes';
+// document.write(P)
+// document.write(name)
+// alert(one)
+// alert(job)
+// console.log(age)
+// console.log(IsWife)
+
+//4 завдання
+// let a=prompt('ваше ім\'я')
+// let b=prompt('ваше призвіще')
+// let c=prompt('по батькові')
+// document.write(a)
+// document.write(b)
+// document.write(c)
+// alert(a)
+// alert(b)
+// alert(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+//5 завдання
+// let name=prompt('ім\'я призвіще по батьку');
+// //? let name=prompt('ім\'я') || prompt('призвіще') || prompt('по батьку');?
+// document.write(name)
+
+//6 завдання
+// let name=prompt('ім\'я призвіще по батьку');
+// let name1=prompt('ім\'я призвіще по батьку батька');
+// let name2=prompt('ім\'я призвіще по батьку мати');
+// let name3=prompt('ім\'я призвіще по батьку при наявності брата , сестри');
+// document.write(name)
+// document.write(name1)
+// document.write(name2)
+// document.write(name3)
+
+//7 завдання
+// let a= + prompt('перше число')
+// let b= + prompt('друге число')
+// let c= + prompt('третє число')
+// console.log+(a)
+// console.log+(b)
+// console.log+(c)
+
+//8 завдання
+// let a= parseInt prompt ('перше число')
+// let b= parseInt prompt ('друге число')
+// let c= parseInt prompt ('третє число')
+// let d= parseInt prompt ('четверте число')
+// let result= +(a+b+c+d)
+// console.log (result)
+
+//9 завдання
+// let a= + prompt ('перше число');
+// let b= + prompt ('друге число');
+// let c= + prompt ('третє число');
+// let result= (a+b+c);
+// console.log(parseFloat(result));
+
+//10завдання
+// let a= + prompt ('перше число');
+// let b= + prompt ('друге число');
+// let c= + prompt ('третє число');
+// let result= (a+b+c)
+// console.log(Math.round(result));
+
+// 11 завдання
+// let a= + prompt ('число що буде возврдитись до степені');
+// let b= + prompt ('степінь');
+// let  result = Math.pow(a, b)
+// console.log(result)
+
+// 12 завдання
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+
+//13 завдання
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 = 6 -> false
+// 5 <= 6 -> false
+// 10 = 10 -> true
+// 10 >= 10 -> true
+// 10 < 10 -> false
+// 10 > 10 -> false
+// 10! = 10 -> false
+// 123 = '123' -> false
+// 123 != '123' -> true
+
+// 14 завдвння
+// console.log(132 > 100 && 45 < 12 );
+// console.log(34 > 33 && 23 < 90 );
+// console.log(99 > 100 && 45 > 12 );
+// console.log(132 > 100 || 45 < 12 );
+// console.log(111 > 11 || 45 < 111 );
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+// console.log(!!'-1');
+// console.log(!!-1);
+// console.log(!!'0');
+// console.log(!!'null');
+// console.log(!!'undefined');
+// console.log(!!(3/'owu'));
+// console.log((111 > 11 || 45 < 111) ||  !!'0');
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+// false
+// true
+// false
+// true
+// true
+// true
+// true
+// false
+// true
+// true
+// true
+// true
+// true
+// true
+// false
+// true
+// false
+
+//                                          CLASS
+
+// 1 завдання
+// let str = "Привіт";
+// let num = 123;
+// let flag = true;
+// let txt = 'true';
+// console.log(typeof(str))
+// console.log(typeof(num))
+// console.log(typeof(flag))
+// console.log(typeof(txt))
+
+// 2 завдання
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+// 3 завдвння
+// let a6 = 5 % 3;
+// let a7 =  3 % 5;
+// let a8 = 5 + '3';
+// let a9 = '5' - 3;
+// let a10 = 75 + 'кг';
+
+//4 завдання
+// let height = + prompt('висота прямокутника(cm)') ;
+// let width = + prompt('ширина прямокутника(cm)') ;
+// let S = height * width;
+// document.write('Площа прямокутника =')
+// document.write(S)
+// document.write('см')
+
+//5 завдання
+// let heightC =  '10m';
+// heightC = parseFloat(heightC)
+// let dC =  '4m';
+// dC = parseFloat(dC)
+//  let P =Math.PI;
+// let dC2 =  Math.pow( (dC/2) , 2);
+// let V=  P * dC2 * heightC;
+// console.log(V);
+
+// 6  завдання
+// let n = 3;
+// let m = 4;
+// let n2 = Math.pow(n , 2);
+// let m2 = Math.pow(m , 2);
+// let k2 =n2 + m2 ;
+// let k = Math.pow(k2 , 0.5)
+// console.log(k)
+
+// 7 завдання
+// let h = 'hello world';
+// alert(h);
+// document.write(h);
+// console.log(h);
+
+// 8 завдання
+// let a = prompt('ФИО');
+// let b = prompt('Вік');
+// let c = prompt('Хобби');
+// alert(a +'\n'+ b +'\n '+ c );
+
+// 9 завдання
+// let str1 ='Кто ';
+// let str2 ='ты ';
+// let str3 ='такой? ';
+// document.write(str1.concat(str2 , str3))   ;
+
+// 10 завдання
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");  205 //томущо str є string
+// document.write(str - a + "<br/>");  15
+// document.write(str * "2" + "<br/>");40
+// document.write(str / 2 + "<br/>");  10
+
+// 11 завдання
+// console.log(parseInt("3.14")); //3
+// console.log(parseInt("-7.875")); //-7
+// console.log(parseInt("435")); //475
+// console.log(parseInt("Вася")); //NaN томущо це не число
+
+// 12 завдання
+// let str = prompt("Enter something", "гітлер в шароварах")
+// console.log(str);
+
+// 13 завдання
+// let a = + prompt('введіть  числo' , '4');
+// let b = + prompt('введіть  числo' , '5');
+// let c = a + b;
+// a = parseFloat(a)
+// b = parseFloat(b)
+// alert(c)
+
+// 14 завдання
+// let a =  prompt('Ім\'я' , 'Валера');
+// let b =  prompt('Фамілія' , 'Кузнець');
+// let c = + prompt('Вік' , '34');
+// alert( 'Доброго вечера ' + a +' ' + b +', мои поздравления что вам ' + c)
